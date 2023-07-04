069f10e46405fbebe780badc15fd2b39c52c8be6
Christopher <christo26.g@gmail.com>
2023-07-04 23:20:11 +0200

    hamburger

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,12 +5,12 @@ import Homepage from './home/Homepage';
 
 const Main = props => (
     <Routes>
-        <Route path="/" element={<Homepage />}/>
-        <Route path="/mes-valeurs" />
-        <Route path="/mes-services" />
-        <Route path="/devis" />
-        <Route path="/a-propos" />
-        <Route path="/contact" />
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Homepage />}/>
+        <Route path={process.env.PUBLIC_URL + "/mes-valeurs"} />
+        <Route path={process.env.PUBLIC_URL + "/mes-services"} />
+        <Route path={process.env.PUBLIC_URL + "/devis"} />
+        <Route path={process.env.PUBLIC_URL + "/a-propos"} />
+        <Route path={process.env.PUBLIC_URL + "/contact"} />
     </Routes>
 )
 
